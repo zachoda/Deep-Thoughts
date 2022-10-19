a811c71d08a9b6b87e7430b6f65a8d90f29f6868
Zach Oda <zjodachow@gmail.com>
2022-10-19 16:42:46 -0400

    integrated front end

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,55 +1,51 @@
-//import the gql tagged template function
-const {gql} = require('apollo-server-express');
+const { gql } = require('apollo-server-express');
 
-//create our TypeDefs
 const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
+  }
 
-    type Thought {
-        _id: ID
-        thoughtText: String
-        createdAt: String
-        username: String
-        reactionCount: Int
-        reactions: [Reaction]
-    }
+  type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+    reactions: [Reaction]
+  }
 
-    type Reaction {
-        _id: ID
-        reactionBody: String
-        createdAt: String
-        username: String
-    }
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+  }
 
-    type User {
-        _id: ID
-        username: String
-        email: String
-        friendCount: Int
-        thoughts: [Thought]
-        friends: [User]
-    }
+  type Auth {
+    token: ID!
+    user: User
+  }
 
-    type Query {
-        me: User
-        users: [User]
-        user(username: String!): User
-        thoughts(username: String): [Thought]
-        thought(_id: ID!): Thought
-    }
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+    thoughts(username: String): [Thought]
+    thought(_id: ID!): Thought
+  }
 
-    type Mutation {
-        login(email: String!, password: String!): Auth
-        addUser(username: String!, email: String!, password: String!): Auth
-        addThought(thoughtText: String!): Thought
-        addReaction(thoughtId: ID!, reactionBody: String!): Thought
-        addFriend(friendId: ID!): User
-    }
-
-    type Auth {
-        token: ID!
-        user: User
-    }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
+  }
 `;
 
-//export the TypeDefs
 module.exports = typeDefs;
--- server/server.js
+++ server/server.js
@@ -1,35 +1,43 @@
 const express = require('express');
-//import ApolloServer
 const {ApolloServer} = require('apollo-server-express');
+const path = require('path');
 
-
-//import our typeDefs and resolvers
 const {typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
-//create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
-  typeDefs, resolvers
+  typeDefs,
+  resolvers,
 });
+
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-//create a new instance of an Apollo server with the GraphQL schema
-const startApolloServer = async(typeDefs, resolvers) => {
-  await server.start();
-// integrate our Apollo server with the Express application as middleware
-server.applyMiddleware({app});
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    //log where we can go to test our GQL API
-    console.log(`Use GraphGQL at http://localhost:${PORT}${server.graphqlPath}`);
-  })
-})
-};
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
-// Call the async function to start the server
-startApolloServer(typeDefs, resolvers);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+  
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+  };
+  
+  // Call the async function to start the server
+  startApolloServer(typeDefs, resolvers);
