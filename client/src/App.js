37bec218b9183532a7fd590ff88f59c6ed07e5eb
Zach Oda <zjodachow@gmail.com>
2022-10-20 19:26:10 -0400

    updated user

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import {
   InMemoryCache,
   createHttpLink,
 } from "@apollo/client";
+import {setContext} from '@apollo/client/link/context';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Login from "./pages/Login";
@@ -20,8 +21,20 @@ import Home from "./pages/Home";
 const httpLink = createHttpLink({
   uri: "/graphql",
 });
+
+
+const authLink = setContext((_, {headers}) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
